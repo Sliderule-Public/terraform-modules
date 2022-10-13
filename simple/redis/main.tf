1e61704473b7a558ef4bc2165a21bfc79de5a7a6
Matt Martinez <matthewmartinez1003@gmail.com>
2022-10-13 11:42:06 -0500

    add replication group

--- simple/redis/main.tf
+++ simple/redis/main.tf
@@ -25,12 +25,30 @@ resource "aws_elasticache_parameter_group" "new" {
 
 resource "aws_elasticache_cluster" "cluster" {
   cluster_id           = substr("${var.company_name}-${var.environment}-${var.name}", 0, 64)
-  engine               = "redis"
-  node_type            = var.node_type
-  num_cache_nodes      = 1
-  parameter_group_name = aws_elasticache_parameter_group.new.name
-  engine_version       = "3.2.10"
-  port                 = 6379
-  security_group_ids = var.security_group_ids
-  subnet_group_name = aws_elasticache_subnet_group.subnet_group.name
+  replication_group_id = aws_elasticache_replication_group.main.id
+  #  engine               = "redis"
+  #  node_type            = var.node_type
+  #  num_cache_nodes      = 1
+  #  parameter_group_name = aws_elasticache_parameter_group.new.name
+  #  engine_version       = "3.2.10"
+  #  port                 = 6379
+  #  security_group_ids   = var.security_group_ids
+  #  subnet_group_name    = aws_elasticache_subnet_group.subnet_group.name
+}
+
+resource "aws_elasticache_replication_group" "main" {
+  automatic_failover_enabled    = true
+  multi_az_enabled              = true
+  engine                        = "redis"
+  preferred_cache_cluster_azs   = var.subnet_ids
+  replication_group_id          = substr("${var.company_name}-${var.environment}-${var.name}", 0, 64)
+  description                   = substr("${var.company_name}-${var.environment}-${var.name}", 0, 64)
+  node_type                     = var.node_type
+  engine_version                = "3.2.10"
+  security_group_ids            = var.security_group_ids
+  subnet_group_name             = aws_elasticache_subnet_group.subnet_group.name
+  num_cache_clusters            = 2
+  parameter_group_name          = aws_elasticache_parameter_group.new.name
+  port                          = 6379
+  replication_group_description = substr("${var.company_name}-${var.environment}-${var.name}", 0, 64)
 }
