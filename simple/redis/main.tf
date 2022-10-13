bf18aa54a887c63a958ad8ff10da46470953bcc1
Matt Martinez <matthewmartinez1003@gmail.com>
2022-10-13 12:21:57 -0500

    add second cluster

--- simple/redis/main.tf
+++ simple/redis/main.tf
@@ -35,6 +35,18 @@ resource "aws_elasticache_cluster" "cluster" {
   #  security_group_ids   = var.security_group_ids
   #  subnet_group_name    = aws_elasticache_subnet_group.subnet_group.name
 }
+resource "aws_elasticache_cluster" "cluster2" {
+  cluster_id           = substr("${var.company_name}-${var.environment}-${var.name}", 0, 64)
+  replication_group_id = aws_elasticache_replication_group.main.id
+  #  engine               = "redis"
+  #  node_type            = var.node_type
+  #  num_cache_nodes      = 1
+  #  parameter_group_name = aws_elasticache_parameter_group.new.name
+  #  engine_version       = "3.2.10"
+  #  port                 = 6379
+  #  security_group_ids   = var.security_group_ids
+  #  subnet_group_name    = aws_elasticache_subnet_group.subnet_group.name
+}
 
 resource "aws_elasticache_replication_group" "main" {
   automatic_failover_enabled    = true
@@ -48,4 +60,5 @@ resource "aws_elasticache_replication_group" "main" {
   parameter_group_name          = aws_elasticache_parameter_group.new.name
   port                          = 6379
   replication_group_description = substr("${var.company_name}-${var.environment}-${var.name}", 0, 64)
+  at_rest_encryption_enabled    = true
 }
