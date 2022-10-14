8c8884bcd365a017120cbb36b32e3c9915361aaf
Matt Martinez <matthewmartinez1003@gmail.com>
2022-10-14 07:38:41 -0500

    update param

--- simple/redis/main.tf
+++ simple/redis/main.tf
@@ -13,7 +13,7 @@ resource "aws_elasticache_parameter_group" "default" {
   }
   parameter {
     name  = "cluster-enabled"
-    value = "true"
+    value = "yes"
   }
 }
 
@@ -27,46 +27,30 @@ resource "aws_elasticache_parameter_group" "new" {
   }
 }
 
-resource "aws_elasticache_cluster" "cluster" {
-  cluster_id           = substr("${var.company_name}-${var.environment}-${var.name}", 0, 64)
-  replication_group_id = aws_elasticache_replication_group.main.id
-  #  engine               = "redis"
-  #  node_type            = var.node_type
-  #  num_cache_nodes      = 1
-  #  parameter_group_name = aws_elasticache_parameter_group.new.name
-  #  engine_version       = "3.2.10"
-  #  port                 = 6379
-  #  security_group_ids   = var.security_group_ids
-  #  subnet_group_name    = aws_elasticache_subnet_group.subnet_group.name
-}
-resource "aws_elasticache_cluster" "cluster2" {
-  cluster_id           = substr("${var.company_name}-${var.environment}-${var.name}2", 0, 64)
-  replication_group_id = aws_elasticache_replication_group.main.id
-  #  engine               = "redis"
-  #  node_type            = var.node_type
-  #  num_cache_nodes      = 1
-  #  parameter_group_name = aws_elasticache_parameter_group.new.name
-  #  engine_version       = "3.2.10"
-  #  port                 = 6379
-  #  security_group_ids   = var.security_group_ids
-  #  subnet_group_name    = aws_elasticache_subnet_group.subnet_group.name
-}
-
-resource "aws_elasticache_replication_group" "main" {
-  automatic_failover_enabled    = true
-  multi_az_enabled              = true
-  engine                        = "redis"
-  replication_group_id          = substr("${var.company_name}-${var.environment}-${var.name}-rg", 0, 64)
-  node_type                     = var.node_type
-  number_cache_clusters         = 2
-  engine_version                = "5.0.6"
-  security_group_ids            = var.security_group_ids
-  subnet_group_name             = aws_elasticache_subnet_group.subnet_group.name
-  parameter_group_name          = aws_elasticache_parameter_group.new.name
-  port                          = 6379
-  replication_group_description = substr("${var.company_name}-${var.environment}-${var.name}-rg", 0, 64)
-  at_rest_encryption_enabled    = true
-}
+#resource "aws_elasticache_cluster" "cluster" {
+#  cluster_id           = substr("${var.company_name}-${var.environment}-${var.name}", 0, 64)
+#  replication_group_id = aws_elasticache_replication_group.main.id
+#}
+#resource "aws_elasticache_cluster" "cluster2" {
+#  cluster_id           = substr("${var.company_name}-${var.environment}-${var.name}2", 0, 64)
+#  replication_group_id = aws_elasticache_replication_group.main.id
+#}
+#
+#resource "aws_elasticache_replication_group" "main" {
+#  automatic_failover_enabled    = true
+#  multi_az_enabled              = true
+#  engine                        = "redis"
+#  replication_group_id          = substr("${var.company_name}-${var.environment}-${var.name}-rg", 0, 64)
+#  node_type                     = var.node_type
+#  number_cache_clusters         = 2
+#  engine_version                = "5.0.6"
+#  security_group_ids            = var.security_group_ids
+#  subnet_group_name             = aws_elasticache_subnet_group.subnet_group.name
+#  parameter_group_name          = aws_elasticache_parameter_group.new.name
+#  port                          = 6379
+#  replication_group_description = substr("${var.company_name}-${var.environment}-${var.name}-rg", 0, 64)
+#  at_rest_encryption_enabled    = true
+#}
 
 resource "aws_elasticache_replication_group" "default" {
   replication_group_id          = substr("${var.company_name}-${var.environment}-${var.name}", 0, 64)
