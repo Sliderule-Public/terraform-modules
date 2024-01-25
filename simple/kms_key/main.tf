7b77c31d29731d1b7a98e29f1065fa4d69653c19
Matt Martinez <matthewmartinez1003@gmail.com>
2024-01-25 13:51:12 -0600

    add depends on

--- simple/kms_key/main.tf
+++ simple/kms_key/main.tf
@@ -1,11 +1,12 @@
 resource "aws_kms_key" "key" {
   deletion_window_in_days = 10
   enable_key_rotation     = true
-  policy = var.policy
-  tags = var.tags
+  policy                  = var.policy
+  tags                    = var.tags
 }
 
 resource "aws_kms_grant" "grant" {
+  depends_on        = [aws_kms_key.key]
   count             = length(var.usage_grantee_arns)
   name              = "${var.company_name}-${var.environment}-${var.key_name}"
   key_id            = aws_kms_key.key.key_id
