f2fac791f5520ccac3831fd0d7f4cb3a17e6b289
Matt Martinez <matthewmartinez1003@gmail.com>
2024-02-14 16:26:36 -0600

    add new bucket output

--- simple/s3_bucket/output.tf
+++ simple/s3_bucket/output.tf
@@ -4,3 +4,10 @@ output "bucket_arn" {
 output "bucket" {
   value = aws_s3_bucket.bucket.bucket
 }
+
+output "crr_bucket_arn" {
+  value = aws_s3_bucket.bucket_main.arn
+}
+output "crr_bucket" {
+  value = aws_s3_bucket.bucket_main.bucket
+}
