af8e74bdf7db2df0d428c0c00c046c45d3301c11
Matt Martinez <matthewmartinez1003@gmail.com>
2024-03-01 06:43:39 -0600

    add green sg

--- simple/vpc_security_group/output.tf
+++ simple/vpc_security_group/output.tf
@@ -1,7 +1,7 @@
 output "security_group_id" {
-  value = aws_security_group.group.id
+  value = aws_security_group.main.id
 }
 
 output "security_group_name" {
-  value = aws_security_group.group.name
+  value = aws_security_group.main.name
 }
