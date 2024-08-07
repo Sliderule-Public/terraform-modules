4241852ca7dac201e3896e139d3f0e466d17e832
Matt Martinez <matthewmartinez1003@gmail.com>
2024-08-07 15:22:57 -0500

    add sqs dlq

--- simple/sqs_queue/main.tf
+++ simple/sqs_queue/main.tf
@@ -6,4 +6,19 @@ resource "aws_sqs_queue" "terraform_queue" {
   receive_wait_time_seconds = 10
   sqs_managed_sse_enabled   = true
   tags                      = var.tags
+  redrive_policy            = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dlq.arn
+    maxReceiveCount     = 4
+  })
+}
+
+resource "aws_sqs_queue" "dlq" {
+  name                      = substr("dlq-${var.company_name}-${var.environment}-${var.name}", 0, 64)
+  message_retention_seconds = 1209600
+  redrive_allow_policy      = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [
+      aws_sqs_queue.terraform_queue.arn
+    ]
+  })
 }
